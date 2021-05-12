fe39cd2d8df0eb750ba5f4e54432be6b17468458
Jasman Singh Sahi <jasman.singh.sahi@gmail.com>
2021-05-12 14:37:21 -0400

    Updates

---
+++ precache-manifest.f8dcbb0fdaa904e7187ab7eca3c23807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87180458fbcfc1bdf8d86c0bbd76b3c8",
+    "url": "/PortfolioWebsiteTest/index.html"
+  },
+  {
+    "revision": "a7447bf5254137bb354b",
+    "url": "/PortfolioWebsiteTest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a40bfdf5cd74db8cee8",
+    "url": "/PortfolioWebsiteTest/static/js/2.6f640be5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/PortfolioWebsiteTest/static/js/2.6f640be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7447bf5254137bb354b",
+    "url": "/PortfolioWebsiteTest/static/js/main.3d3b029c.chunk.js"
+  },
+  {
+    "revision": "3b803b372a99b48453be",
+    "url": "/PortfolioWebsiteTest/static/js/runtime-main.cdfb7ce7.js"
+  }
+]);
